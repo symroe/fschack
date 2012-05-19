67ccff1ba3be7ab7199c4779e1293c3d7b917659
Sym Roe <sym.roe@talusdesign.co.uk>
2012-05-19 19:34:14 +0100

    add report IDS

--- storage/datastore/management/commands/import_json.py
+++ storage/datastore/management/commands/import_json.py
@@ -1,5 +1,6 @@
 import sys
 import json
+import glob
 
 from django.conf import settings
 from django.core.management.base import BaseCommand, CommandError
@@ -11,12 +12,13 @@ class Command(BaseCommand):
     def add_doc(self, doc):
         reports.save(doc)
     
-    def handle(self, **options):
-        infile = sys.stdin.read()
-        data = json.loads(infile)
-        if isinstance(data, list):
-            for doc in data:
-                self.add_doc(doc)
-        else:
-            self.add_doc(data)
+    def handle(self, path, **options):
+        in_path = "%s*.json" % path
+        for f in glob.glob(in_path):
+            data = json.loads(open(f).read())
+            if isinstance(data, list):
+                for doc in data:
+                    self.add_doc(doc)
+            else:
+                self.add_doc(data)
         
