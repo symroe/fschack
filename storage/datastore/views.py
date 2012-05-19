149c6dc8efc16d3f41d9c3d267176f700b913413
Sym Roe <sym.roe@talusdesign.co.uk>
2012-05-19 17:42:24 +0100

    saving some data now

--- storage/datastore/views.py
+++ storage/datastore/views.py
@@ -14,6 +14,188 @@ def json_response(res, code=200):
 
 class AddReport(View):
     def get(self, request):
+        
+        return HttpResponse("""
+        <form method="post" enctype="text/plain">
+        <textarea name="asd">
+    
+            [
+                {
+                    "user_id": "Group1",
+                    "group_id": "Group1",
+                    "datetime": "2012-01-01",
+                    "experiment_type": "Wet Width",
+                    "location": {
+                        "name": "Site1"
+                    },
+                    "notes": "",
+                    "data": {
+                        "measurement": 1.9,
+                        "unitofmeasurement": "m"
+                    }
+                },
+                {
+                    "user_id": "Group1",
+                    "group_id": "Group1",
+                    "datetime": "2012-01-01",
+                    "experiment_type": "Wetted Perimeter",
+                    "location": {
+                        "name": "Site1"
+                    },
+                    "notes": "",
+                    "data": {
+                        "measurement": 2.38,
+                        "unitofmeasurement": "m"
+                    }
+                },
+                {
+                    "user_id": "Group1",
+                    "group_id": "Group1",
+                    "datetime": "2012-01-01",
+                    "experiment_type": "Gradient",
+                    "location": {
+                        "name": "Site1"
+                    },
+                    "notes": "",
+                    "data": {
+                        "measurement": 4,
+                        "unitofmeasurement": "degree"
+                    }
+                },
+                {
+                    "user_id": "Group1",
+                    "group_id": "Group1",
+                    "datetime": "2012-01-01",
+                    "experiment_type": "Impellor Time",
+                    "location": {
+                        "name": "Site1"
+                    },
+                    "notes": "",
+                    "data": {
+                        "measurement": [
+                            75,
+                            28,
+                            20,
+                            19.5,
+                            78
+                        ],
+                        "unitofmeasurement": "seconds"
+                    }
+                },
+                {
+                    "user_id": "Group1",
+                    "group_id": "Group1",
+                    "datetime": "2012-01-01",
+                    "experiment_type": "Wet (Water) Depth",
+                    "location": {
+                        "name": "Site1"
+                    },
+                    "notes": "",
+                    "data": {
+                        "measurement": [
+                            0.2,
+                            0.25,
+                            0.11,
+                            0.1,
+                            0.1,
+                            0.09,
+                            0.06,
+                            0.12,
+                            0.02,
+                            0.03
+                        ],
+                        "unitofmeasurement": "m"
+                    }
+                },
+                {
+                    "user_id": "Group1",
+                    "group_id": "Group1",
+                    "datetime": "2012-01-01",
+                    "experiment_type": "Stone Measurement",
+                    "location": {
+                        "name": "Site1"
+                    },
+                    "notes": "",
+                    "data": [
+                        {
+                            "length": 99,
+                            "width": 71,
+                            "depth": 8,
+                            "powershapeindex": 4
+                        },
+                        {
+                            "length": 102,
+                            "width": 97,
+                            "depth": 37,
+                            "powershapeindex": 4
+                        },
+                        {
+                            "length": 91,
+                            "width": 34,
+                            "depth": 5,
+                            "powershapeindex": 3
+                        },
+                        {
+                            "length": 56,
+                            "width": 20,
+                            "depth": 9,
+                            "powershapeindex": 4
+                        },
+                        {
+                            "length": 91,
+                            "width": 52,
+                            "depth": 19,
+                            "powershapeindex": 4
+                        },
+                        {
+                            "length": 52,
+                            "width": 25,
+                            "depth": 10,
+                            "powershapeindex": 4
+                        },
+                        {
+                            "length": 67,
+                            "width": 31,
+                            "depth": 10,
+                            "powershapeindex": 4
+                        },
+                        {
+                            "length": 69,
+                            "width": 36,
+                            "depth": 5,
+                            "powershapeindex": 4
+                        },
+                        {
+                            "length": 55,
+                            "width": 35,
+                            "depth": 13,
+                            "powershapeindex": 4
+                        },
+                        {
+                            "length": 34,
+                            "width": 30,
+                            "depth": 8,
+                            "powershapeindex": 3
+                        },
+                        {
+                            "length": 46,
+                            "width": 35,
+                            "depth": 7,
+                            "powershapeindex": 5
+                        }
+                    ]
+                }
+            ]
+    
+    
+    
+        </textarea>
+        <input type="submit" /></form>
+        
+        
+        """)
+        
+        
         res = json.dumps({
             'error' : """Use POST or PUT methods to add or update a report."""
         })
@@ -25,8 +207,8 @@ class AddReport(View):
         
         Always returns True for now
         """
-        if 'data' not in report:
-            return {"error":"No data found"}
+        # if 'data' not in report:
+        #     return {"error":"No data found"}
         
         
         return False
@@ -71,10 +253,18 @@ class AddReport(View):
         
         
         """
+        
+        print repr(request.raw_post_data)
+        print request.POST
+        
         try:
-            data = json.loads(request.raw_post_data)
-        except:
+            data = json.loads(request.raw_post_data[4:])
+        except Exception, e:
+            print e
             data = ""
+        
+        print repr(request.raw_post_data)
+        print repr(data)
 
         ids = []
         if isinstance(data, list):
